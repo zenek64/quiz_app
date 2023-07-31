26626cea80339369e25c21a413c1803e664e0eb8
Grzegorz <46374229+zenek64@users.noreply.github.com>
2023-07-31 23:20:50 +0200

    empty_page

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/views/start.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: StartPage(),
+      ),
+    ),
+  );
+}
---
+++ lib/views/start.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class StartPage extends StatelessWidget {
+  const StartPage({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(color: Color.fromARGB(255, 13, 71, 161)),
+    );
+  }
+}
